15a3144130bd8acf4481a70f0d880d67251a5d33
Chee Yi Xu <chee0109@e.ntu.edu.sg>
2016-10-09 22:37:35 +0800

    Added CSVUtils class

--- BItest/src/javabitest/JavaBITest.java
+++ BItest/src/javabitest/JavaBITest.java
@@ -1,5 +1,33 @@
 package javabitest;
 
+import java.math.BigInteger;
+import java.time.*;
+
 public class JavaBITest {
+
+	public static void main(String[] args) {
+
+		Instant start = Instant.now();
+
+		BigInteger bi1 = new BigInteger(
+				"98127398749871298379182739128378127819284738192487023985092384092839482382375981729837120983012983098");
+		BigInteger bi2 = new BigInteger("368172389719283784782379487238947239847923487239847987");
+
+		BigInteger bi3 = bi1.add(bi2);
+
+		for (long i = 0; i < (int)Math.pow(2, 64) - 1; i++) {
+
+			bi3.add(bi3);
+
+		}
+
+		Instant end = Instant.now();
+
+		Duration duration = Duration.between(start, end);
+
+		System.out.println(bi3);
+		System.out.println(duration.toMillis());
+
+	}
 
 }
